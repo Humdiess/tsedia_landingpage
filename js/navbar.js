3b73bfb8f03964ca899d6165b659848a736f890f
Humdiess <hmzinchannel@gmail.com>
2024-10-16 14:02:52 +0700

    style: navbar active state

--- js/navbar.js
+++ js/navbar.js
@@ -32,3 +32,24 @@ window.addEventListener('scroll', function() {
     toggleNavItems(window.scrollY);
     toggleLogoVisibility(window.scrollY);
   });
+
+
+
+// active state
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.js-mobile-menu nav ul li a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
